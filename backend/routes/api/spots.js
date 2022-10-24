fa333711047514588542abd3cd7901e0e9437530
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-10-24 17:46:19 -0500

    Add POST /api/spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, Review, SpotImage, User, sequelize } = require('../../db/models');
 
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const jwt = require('jsonwebtoken');
@@ -41,13 +41,12 @@ async function getSpots(args) {
 }
 
 router.get('/current', async (req, res) => {
-    const currentUserId = jwt.decode(req.cookies.token).data.id;
-    res.json(await getSpots({ currentUserId }));
-})
+    res.json(await getSpots({ currentUserId: req.user.id }));
+});
 
 router.get('/', async (req, res) => {
     res.json(await getSpots());
-})
+});
 
 router.get('/:spotId', async (req, res) => {
     const options = {
@@ -78,6 +77,58 @@ router.get('/:spotId', async (req, res) => {
             "statusCode": 404
         });
     }
-})
+});
+
+const validateSpotCreation = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .withMessage('Longitude is required'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Name is required')
+        .isLength({ max: 49 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage('Price per day is required'),
+];
+
+router.post('/', requireAuth, validateSpotCreation, async (req, res) => {
+    const errorObjects = validationResult(req);
+    if (errorObjects.isEmpty()) {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        const spot = await Spot.create({ ownerId: req.user.id, address, city, state, country, lat, lng, name, description, price });
+        res.status(201).json(spot);
+    }
+    else {
+        const errors = errorObjects.array().reduce((errors, errObj) => {
+            errors[errObj.param] = errObj.msg;
+            return errors;
+        }, {});
+        res.status(400).json({
+            message: "Validation Error",
+            statusCode: 400,
+            errors
+        });
+    }
+});
 
 module.exports = router;
