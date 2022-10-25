165ef71134f6e59d68b8579ff741a81ba71dcfb1
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-10-25 13:15:57 -0500

    Add GET /api/reviews/current

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,11 @@ const isProduction = environment === 'production';
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+})
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
