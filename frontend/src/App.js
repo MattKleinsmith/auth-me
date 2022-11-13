b7e9c30528eb849da3620f45a22c537b8ce13398
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-12 20:56:21 -0600

    Add favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
+import Header from "./components/Header";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <Navigation isLoaded={isLoaded} />
+      <Header isLoaded={isLoaded} />
     </>
   );
 }
