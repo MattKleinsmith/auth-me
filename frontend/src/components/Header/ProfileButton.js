11475827d85df3a5049b21ac231f08a165a86d49
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-12 23:56:36 -0600

    Style profile button

--- frontend/src/components/Header/ProfileButton.js
+++ frontend/src/components/Header/ProfileButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import "./ProfileButton.css";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -30,7 +31,8 @@ function ProfileButton({ user }) {
 
     return (
         <>
-            <button onClick={openMenu}>
+            <button onClick={openMenu} className="profileButton">
+                <i class="fa-solid fa-bars"></i>
                 <i className="fas fa-user-circle" />
             </button>
             {showMenu && (
