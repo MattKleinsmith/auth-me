2a3bda3e2a3082783423ad564b7bc102e78a8788
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-14 10:44:26 -0600

    Style SpotGridItem

--- frontend/src/components/SpotGrid/SpotGridItem.js
+++ frontend/src/components/SpotGrid/SpotGridItem.js
@@ -3,13 +3,15 @@ export default function SpotGridItem() {
         <div className="SpotGridItem">
             <img src="https://a0.muscache.com/im/pictures/miso/Hosting-648913477943796142/original/63f9b8e6-ce57-41aa-97f0-fac7a8e8e2ad.jpeg?im_w=720" />
             <div className="SpotGridItemDescription">
-                <div className="SpotGridItemDescriptionFirstRow">
+                <div className="SpotGridItemFirstRow">
                     <div><strong>Eureka Springs, Arkansas</strong></div>
-                    <div>4.94</div>
+                    <div className="SpotGridItemStarRating"><i class="fa-solid fa-star SpotGridItemStar" /> 4.94</div>
                 </div>
-                <div>185 miles away</div>
-                <div>Feb 5 – 10</div>
-                <div><strong>$189</strong> night</div>
+                <div className="SpotGridItemSubtext">
+                    <div>185 miles away</div>
+                    <div>Feb 5 – 10</div>
+                </div>
+                <div className="SpotGridItemPrice"><strong>$189</strong> night</div>
             </div>
         </div>
     );
