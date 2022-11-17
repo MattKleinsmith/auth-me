682ed89a142f3fe1544d5e553bbbabac25ac0d5c
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-17 10:34:39 -0600

    Don't let owner review their own spot

--- frontend/src/components/SpotDetails/Reviews.js
+++ frontend/src/components/SpotDetails/Reviews.js
@@ -7,8 +7,9 @@ export default function Reviews({ spot, reviews }) {
     const user = useSelector(state => state.session.user);
     if (!reviews) return;
     const userReviewed = reviews.some(review => review.User.id === user.id);
+    const userOwnsSpot = spot.ownerId === user.id;
     return <div>
-        <ReviewHeader spot={spot} userReviewed={userReviewed} />
+        <ReviewHeader spot={spot} userReviewed={userReviewed} userOwnsSpot={userOwnsSpot} />
         <div className="reviews">
             {reviews.map((review, i) => <Review key={i} review={review} user={user} />)}
         </div>
