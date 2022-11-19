0be023f0e0ed20c002af948ce9e770cc48ee79fa
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-11-19 16:25:36 -0600

    Apply media queries to Header on homepage

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import './Header.css';
 import Logo from './Logo';
@@ -6,16 +7,22 @@ import RightHeader from './RightHeader/RightHeader'
 
 export default function Header() {
     const ui = useSelector(state => state.ui);
-
+    const history = useHistory();
+    console.log("on spot details?", history.location.pathname.includes("spot"));
     return (
         <div className="headerWrapper" style={{ position: ui.headerPosition }}>
-            <div style={{ paddingLeft: ui.padding.left, paddingRight: ui.padding.right }}>
+            <div className="homepagePadding" style=
+                {
+                    history.location.pathname.includes("spot") ?
+                        { paddingLeft: ui.padding.left, paddingRight: ui.padding.right } :
+                        {}
+                }>
                 <div className="header">
                     <Logo />
                     <RightHeader />
                 </div>
             </div>
             <div className="line"></div>
-        </div>
+        </div >
     );
 }
